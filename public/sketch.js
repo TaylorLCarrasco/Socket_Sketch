3c0f59adbb19c1ff674efd35225e9bfc0bf65fba
TaylorLCarrasco <50971474+TaylorLCarrasco@users.noreply.github.com>
2021-11-20 19:56:33 -0800

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,14 +1,4 @@
-import { initializeApp } from 'firebase/app';
 
-const firebaseApp = initializeApp({
-    apiKey: "AIzaSyC4cQmnCHXvkDLE6QISv4RSv59EDQnZiCc",
-    authDomain: "socket-sketch-7355b.firebaseapp.com",
-    databaseURL: "https://socket-sketch-7355b-default-rtdb.firebaseio.com",
-    projectId: "socket-sketch-7355b",
-    storageBucket: "socket-sketch-7355b.appspot.com",
-    messagingSenderId: "1017385164549",
-    appId: "1:1017385164549:web:4e28c02faecadefc77616c"
-});
 
 var socket;
 var dataLocal;
@@ -40,6 +30,18 @@ function setup()
     socket = io.connect('https://socket-sketch-app.herokuapp.com/');
     socket.on('mouse', newDrawing);
     socket.on('clear', clearCanvas);
+
+    import { initializeApp } from 'firebase/app';
+
+    const firebaseApp = initializeApp({
+        apiKey: "AIzaSyC4cQmnCHXvkDLE6QISv4RSv59EDQnZiCc",
+        authDomain: "socket-sketch-7355b.firebaseapp.com",
+        databaseURL: "https://socket-sketch-7355b-default-rtdb.firebaseio.com",
+        projectId: "socket-sketch-7355b",
+        storageBucket: "socket-sketch-7355b.appspot.com",
+        messagingSenderId: "1017385164549",
+        appId: "1:1017385164549:web:4e28c02faecadefc77616c"
+    });
 }
 
 function upload()
