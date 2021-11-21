389ac2b0b1692a856a23e5b2d2f7a5cf72f20397
TaylorLCarrasco <50971474+TaylorLCarrasco@users.noreply.github.com>
2021-11-20 19:24:17 -0800

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,7 @@
 //import firebase from 'firebase/compat/app';
 //import 'firebase/compat/auth';
 //import 'firebase/compat/firestore';
-import firebase from "firebase/compat/app";
+import { initializeApp } from "firebase/app"
 
 var socket;
 var dataLocal;
