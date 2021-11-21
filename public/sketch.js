a74c337dc8e9ae5f424b5d10c3e07b17d8cc5516
TaylorLCarrasco <50971474+TaylorLCarrasco@users.noreply.github.com>
2021-11-20 16:55:21 -0800

    u

--- public/sketch.js
+++ public/sketch.js
@@ -29,20 +29,8 @@ function setup()
     socket = io.connect('https://socket-sketch-app.herokuapp.com/');
     socket.on('mouse', newDrawing);
     socket.on('clear', clearCanvas);
-    
-    import { initializeApp } from "firebase/app";
-    
-    var firebaseConfig  = {
-        apiKey: "AIzaSyC4cQmnCHXvkDLE6QISv4RSv59EDQnZiCc",
-        authDomain: "socket-sketch-7355b.firebaseapp.com",
-        databaseURL: "https://socket-sketch-7355b-default-rtdb.firebaseio.com",
-        projectId: "socket-sketch-7355b",
-        storageBucket: "socket-sketch-7355b.appspot.com",
-        messagingSenderId: "1017385164549",
-        appId: "1:1017385164549:web:4e28c02faecadefc77616c"
-    };
-    const app = initializeApp(firebaseConfig);
-    //database = firebase.database();
+        
+    database = app.database();
     
 }
 
