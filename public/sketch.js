47d87cc4df5e82938230cf61518895a47ac3cada
TaylorLCarrasco <50971474+TaylorLCarrasco@users.noreply.github.com>
2021-11-20 20:19:50 -0800

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -31,7 +31,7 @@ function setup()
 
     import { initializeApp } from 'firebase-app';
 
-    const firebaseApp = initializeApp({
+    const firebaseConfig = {
         apiKey: "AIzaSyC4cQmnCHXvkDLE6QISv4RSv59EDQnZiCc",
         authDomain: "socket-sketch-7355b.firebaseapp.com",
         databaseURL: "https://socket-sketch-7355b-default-rtdb.firebaseio.com",
@@ -39,7 +39,11 @@ function setup()
         storageBucket: "socket-sketch-7355b.appspot.com",
         messagingSenderId: "1017385164549",
         appId: "1:1017385164549:web:4e28c02faecadefc77616c"
-    });
+    };
+
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
 }
 
 function upload()
