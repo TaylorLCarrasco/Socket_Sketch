23ca211693f3c78dea9a4e6441111338706ef353
TaylorLCarrasco <50971474+TaylorLCarrasco@users.noreply.github.com>
2021-11-20 19:54:30 -0800

    bn

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyC4cQmnCHXvkDLE6QISv4RSv59EDQnZiCc",
+    authDomain: "socket-sketch-7355b.firebaseapp.com",
+    databaseURL: "https://socket-sketch-7355b-default-rtdb.firebaseio.com",
+    projectId: "socket-sketch-7355b",
+    storageBucket: "socket-sketch-7355b.appspot.com",
+    messagingSenderId: "1017385164549",
+    appId: "1:1017385164549:web:4e28c02faecadefc77616c"
+});
+
 var socket;
 var dataLocal;
 
