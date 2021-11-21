892e3dd88904a6f03d0a65c52300350bcb5802e0
TaylorLCarrasco <50971474+TaylorLCarrasco@users.noreply.github.com>
2021-11-20 20:33:52 -0800

    g

--- public/sketch.js
+++ public/sketch.js
@@ -28,22 +28,6 @@ function setup()
     socket = io.connect('https://socket-sketch-app.herokuapp.com/');
     socket.on('mouse', newDrawing);
     socket.on('clear', clearCanvas);
-
-    import { initializeApp } from 'firebase-app';
-
-    const firebaseConfig = {
-        apiKey: "AIzaSyC4cQmnCHXvkDLE6QISv4RSv59EDQnZiCc",
-        authDomain: "socket-sketch-7355b.firebaseapp.com",
-        databaseURL: "https://socket-sketch-7355b-default-rtdb.firebaseio.com",
-        projectId: "socket-sketch-7355b",
-        storageBucket: "socket-sketch-7355b.appspot.com",
-        messagingSenderId: "1017385164549",
-        appId: "1:1017385164549:web:4e28c02faecadefc77616c"
-    };
-
-
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
 }
 
 function upload()
